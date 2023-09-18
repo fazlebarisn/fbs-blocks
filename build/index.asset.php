f148a200c335d6e70344d5991284c7b02b782716
Fazle Bari <fazlebarisn@gmail.com>
2023-09-18 09:43:46 +0600

    Start

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '0a1c9a3fd6e57f3a78d6');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => 'a503016619f30d7e1c6e');
