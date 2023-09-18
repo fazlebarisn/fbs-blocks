a01dd71c631fc2a41116d867c98f66506f4f69a0
Fazle Bari <fazlebarisn@gmail.com>
2023-09-18 09:57:14 +0600

    constance define

--- fbs-gutenberg-blocks.php
+++ fbs-gutenberg-blocks.php
@@ -13,6 +13,22 @@
  * @package           fbs-blocks
  */
 
+ if ( ! defined( 'FBS_GB_DIR_PATH' ) ) {
+	define( 'FBS_GB_DIR_PATH', __DIR__ );
+}
+
+define( 'FBS_GB_BLOCKS_FILE' , __FILE__ );
+define( 'FBS_GB_BLOCKS_URL' , plugins_url( '' , FBS_GB_BLOCKS_FILE ) );
+define( 'FBS_BLOCK_ASSETS' , FBS_GB_BLOCKS_URL . '/assets' );
+
+
+require_once FBS_GB_DIR_PATH . '/inc/helpers/autoloader.php';
+
+function fbs_gb_blocks_plugin_instance() {
+	\FBS_GB_BLOCKS\Inc\FBS_GB_BLOCKS::get_instance();
+}
+
+fbs_gb_blocks_plugin_instance();
 
 function create_block_fbs_gutenberg_blocks_block_init() {
 	register_block_type( __DIR__ . '/build' );
