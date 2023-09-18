e6991cad5e6272ee32688f84b3c69bdcefb36ddb
Fazle Bari <fazlebarisn@gmail.com>
2023-09-18 10:47:47 +0600

    add descreptyion

--- build/pricing-table/index.asset.php
+++ build/pricing-table/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '85a8a738c19abf3dc346');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '9c94fe8544553d6ef22b');
